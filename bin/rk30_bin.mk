c7d48f169d65dc0ddea8516fbc2f6651d2efbc58
AZ technology <aztec@localhost.domain>
2014-10-17 18:04:59 +0800

    Add prebuilt e2fs utils to ramdisk

--- bin/rk30_bin.mk
+++ bin/rk30_bin.mk
@@ -1,4 +1,6 @@
 PRODUCT_COPY_FILES += \
 	device/rockchip/common/bin/io:system/xbin/io \
 	device/rockchip/common/bin/mkdosfs:root/sbin/mkdosfs \
+	device/rockchip/common/bin/mke2fs:root/sbin/mke2fs \
+	device/rockchip/common/bin/e2fsck:root/sbin/e2fsck \
 	device/rockchip/common/bin/busybox:/system/bin/busybox
