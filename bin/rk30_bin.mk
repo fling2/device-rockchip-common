d7d243ad8757f985594a8af2fcea5d564e77f15e
rochy2014 <rochy.rong@gmail.com>
2015-02-03 19:50:45 +0800

    Add prebuilt resize2fs utils to ramdisk

--- bin/rk30_bin.mk
+++ bin/rk30_bin.mk
@@ -3,4 +3,5 @@ PRODUCT_COPY_FILES += \
 	device/rockchip/common/bin/mkdosfs:root/sbin/mkdosfs \
 	device/rockchip/common/bin/mke2fs:root/sbin/mke2fs \
 	device/rockchip/common/bin/e2fsck:root/sbin/e2fsck \
+	device/rockchip/common/bin/resize2fs:root/sbin/resize2fs \
 	device/rockchip/common/bin/busybox:/system/bin/busybox
